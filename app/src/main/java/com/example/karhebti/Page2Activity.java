71f90f567c7625e7b66d4a4478f2bdd8c223af19
firas.houaidi@tek-up.de firas houaidi <houaidifiras.tekup@gmail.com>
2024-02-09 23:51:22 +0100

    ajout des model a chaque marque fixed

--- app/src/main/java/com/example/karhebti/Page2Activity.java
+++ app/src/main/java/com/example/karhebti/Page2Activity.java
@@ -9,26 +9,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Page2Activity extends AppCompatActivity {
 
-    private Spinner modeleSpinner; // Déclarer le Spinner modeleSpinner au niveau de la classe
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page2);
 
-        Spinner spinner = findViewById(R.id.spinner_marque);
-        modeleSpinner = findViewById(R.id.spinner_modele); // Initialiser le Spinner modeleSpinner
+        // Récupérer les Spinners
+        Spinner spinner_marque = findViewById(R.id.spinner_marque);
+        Spinner spinner_modele = findViewById(R.id.spinner_modele);
 
+        // Récupérer les marques de voiture depuis les ressources
         String[] carBrands = getResources().getStringArray(R.array.car_brands);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, carBrands);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        // Créer un ArrayAdapter pour les marques de voiture
+        ArrayAdapter<String> adapterMarques = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, carBrands);
+        adapterMarques.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Lier l'ArrayAdapter au Spinner des marques
+        spinner_marque.setAdapter(adapterMarques);
+
+        // Définir un écouteur d'événements sur le Spinner des marques
+        spinner_marque.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedBrand = (String) parent.getItemAtPosition(position);
-                loadModelsForBrand(selectedBrand); // Appeler la méthode loadModelsForBrand ici
+                // Récupérer la marque sélectionnée
+                String selectedBrand = parent.getItemAtPosition(position).toString();
+
+                // Charger les modèles correspondant à la marque sélectionnée
+                int modelsResourceId = getResources().getIdentifier(selectedBrand.toLowerCase() + "_models", "array", getPackageName());
+                String[] carModels = getResources().getStringArray(modelsResourceId);
+
+                // Créer un ArrayAdapter pour les modèles de voiture
+                ArrayAdapter<String> adapterModeles = new ArrayAdapter<>(Page2Activity.this, android.R.layout.simple_spinner_item, carModels);
+                adapterModeles.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // Lier l'ArrayAdapter au Spinner des modèles
+                spinner_modele.setAdapter(adapterModeles);
             }
 
             @Override
@@ -36,18 +52,5 @@ public class Page2Activity extends AppCompatActivity {
                 // Ne rien faire si aucune marque n'est sélectionnée
             }
         });
-    }
-
-    // Déplacer la méthode loadModelsForBrand à l'extérieur de la méthode onCreate
-    private void loadModelsForBrand(String brand) {
-        int modelsResId = getResources().getIdentifier(brand.toLowerCase() + "_models", "array", getPackageName());
-        if (modelsResId != 0) {
-            String[] models = getResources().getStringArray(modelsResId);
-            ArrayAdapter<String> modeleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, models);
-            modeleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            modeleSpinner.setAdapter(modeleAdapter);
-        } else {
-            modeleSpinner.setAdapter(null);
-        }
     }
 }
